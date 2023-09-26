2fcb867c8cf13e29be6d0b541ec19857d549938d
srmahour <sadeepmahour@gmail.com>
2023-09-26 20:19:25 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      primary:{
+        100:'#C5C7CA',
+        200:'#7F8084',
+        300:'#6B6C70',
+        400:'#35373B',
+        500:'#27292D',
+        600:'#191920',
+        700:'#131319',
+      },
+      secondary:'#4A96FF'
+    },
+    fontFamily: {
+      sans: ["Inter var, sans-serif",],
+    },
+  },
+  plugins: [],
+}
